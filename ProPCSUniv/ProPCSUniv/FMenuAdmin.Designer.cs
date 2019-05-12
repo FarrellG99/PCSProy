a6030c64c88f4ab991985e2312620a3696925721
Farrell Gunawan <39336308+FarrellG99@users.noreply.github.com>
2019-05-12 13:57:43 +0700

    File Master PCS

---
+++ ProPCSUniv/ProPCSUniv/FMenuAdmin.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ProPCSUniv
+{
+    partial class FMenuAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.masterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.masterJurusanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.masterMahasiswaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.masterDosenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.masterRuanganToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.masterToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(670, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // masterToolStripMenuItem
+            // 
+            this.masterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.masterJurusanToolStripMenuItem,
+            this.masterMahasiswaToolStripMenuItem,
+            this.masterDosenToolStripMenuItem,
+            this.masterRuanganToolStripMenuItem});
+            this.masterToolStripMenuItem.Name = "masterToolStripMenuItem";
+            this.masterToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.masterToolStripMenuItem.Text = "Master";
+            // 
+            // masterJurusanToolStripMenuItem
+            // 
+            this.masterJurusanToolStripMenuItem.Name = "masterJurusanToolStripMenuItem";
+            this.masterJurusanToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.masterJurusanToolStripMenuItem.Text = "Master Jurusan";
+            this.masterJurusanToolStripMenuItem.Click += new System.EventHandler(this.masterJurusanToolStripMenuItem_Click);
+            // 
+            // masterMahasiswaToolStripMenuItem
+            // 
+            this.masterMahasiswaToolStripMenuItem.Name = "masterMahasiswaToolStripMenuItem";
+            this.masterMahasiswaToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.masterMahasiswaToolStripMenuItem.Text = "Master Mahasiswa";
+            this.masterMahasiswaToolStripMenuItem.Click += new System.EventHandler(this.masterMahasiswaToolStripMenuItem_Click);
+            // 
+            // masterDosenToolStripMenuItem
+            // 
+            this.masterDosenToolStripMenuItem.Name = "masterDosenToolStripMenuItem";
+            this.masterDosenToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.masterDosenToolStripMenuItem.Text = "Master Dosen";
+            this.masterDosenToolStripMenuItem.Click += new System.EventHandler(this.masterDosenToolStripMenuItem_Click);
+            // 
+            // masterRuanganToolStripMenuItem
+            // 
+            this.masterRuanganToolStripMenuItem.Name = "masterRuanganToolStripMenuItem";
+            this.masterRuanganToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.masterRuanganToolStripMenuItem.Text = "Master Ruangan";
+            this.masterRuanganToolStripMenuItem.Click += new System.EventHandler(this.masterRuanganToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(171, 22);
+            this.toolStripMenuItem1.Text = "Master Periode";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // FMenuAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(670, 312);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FMenuAdmin";
+            this.Text = "Menu Admin";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem masterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem masterJurusanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem masterMahasiswaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem masterDosenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem masterRuanganToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
---
+++ ProPCSUniv/ProPCSUniv/MasterJurusan.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace ProPCSUniv
+{
+    public partial class MasterJurusan : Form
+    {
+        OracleDataAdapter ADAP;
+        DataTable DT, DTKAJUR;
+        OracleConnection conn = Form1.conn;
+        String searchtxt = "", status;
+        public MasterJurusan()
+        {
+            InitializeComponent();
+        }
+        public void rename_header()
+        {
+            DG.Columns[0].HeaderText = "KODE";
+            DG.Columns[1].HeaderText = "JURUSAN";
+            DG.Columns[2].HeaderText = "NIP";
+            DG.Columns[3].HeaderText = "DOSEN";
+            DG.Columns[4].HeaderText = "STATUS";
+        }
+        private void buka_grid()
+        {
+            DT = new DataTable();
+            ADAP = new OracleDataAdapter("select jurusan.kode_jurusan,nama_jurusan,jurusan.nip,nama_dosen,statusjurusan "+
+                " from jurusan,dosen where jurusan.nip=dosen.nip " + searchtxt, conn);
+            ADAP.Fill(DT);
+            DG.DataSource = DT;
+            rename_header();
+        }
+        private void buka_kajur()
+        {
+            DTKAJUR = new DataTable();
+            ADAP = new OracleDataAdapter("select * from dosen", conn);
+            ADAP.Fill(DTKAJUR);
+            cmbKepalaJur.Items.Clear();
+            foreach (DataRow item in DTKAJUR.Rows)
+            {
+                cmbKepalaJur.Items.Add(item[1].ToString());
+            }
+        }
+        private int cari_idx_kajur(String kd)
+        {
+            int pos = -1;
+            for (int i = 0; i < DTKAJUR.Rows.Count; i++)
+            {
+                if (DTKAJUR.Rows[i][0].ToString() == kd) pos = i;
+            }
+            return pos;
+        }
+        public void siapkan_form_mode(Boolean bol) //patokan dari button insert
+        {
+            btnInsert.Enabled = bol;
+            btnUpdate.Enabled = !bol;
+            btnDelete.Enabled = !bol;
+            txtKodeJur.Enabled = bol;
+        }
+        public void bersihkan_form()
+        {
+            txtKodeJur.Text = ""; txtNamaJur.Text = ""; cmbKepalaJur.SelectedIndex = -1;
+            rbAktif.Checked = true;
+        }
+        private void MasterJurusan_Load(object sender, EventArgs e)
+        {
+            buka_grid(); buka_kajur(); siapkan_form_mode(true);
+        }
+
+        private void btnCari_Click(object sender, EventArgs e)
+        {
+            searchtxt = " and (upper(nama_jurusan) like '%" + txtSearch.Text.ToUpper() + "%' " +
+                " or upper(nama_dosen) like '%" + txtSearch.Text.ToUpper() + "%' or jurusan.kode_jurusan like '%"+ 
+                txtSearch.Text.ToUpper()+ "%') order by 1";
+            buka_grid();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (rbTidakAktif.Checked) status = "N";
+            else status = "Y";
+            if (txtKodeJur.Text == "") MessageBox.Show("Isi Kode Jurusan");
+            else if (txtNamaJur.Text == "") MessageBox.Show("Isi Nama Jurusan");
+            else if (cmbKepalaJur.SelectedIndex == -1) MessageBox.Show("Pilih Kepala Jurusan");
+            else
+            {
+                try
+                {
+                    OracleCommand oupd = new OracleCommand("update jurusan set " +
+                     "nip='" + DTKAJUR.Rows[cmbKepalaJur.SelectedIndex].ItemArray[0].ToString() + "'," +
+                     "nama_jurusan='" + txtNamaJur.Text + "'," +
+                     "statusjurusan='" + status + "' where " +
+                     "kode_jurusan='" + txtKodeJur.Text + "'"
+                     , conn);
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+                    oupd.ExecuteNonQuery();
+                    MessageBox.Show("Data jurusan terubah");
+                    siapkan_form_mode(true);
+                    buka_grid(); bersihkan_form();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal Update : " + ex.Message);
+                }
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            /*try
+            {
+                OracleCommand oupd = new OracleCommand("update jurusan set " +                   
+                    "statusjurusan='N' where " +
+                    "kodejurusan='" + txtKodeJur.Text + "'"
+                    , conn);
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                oupd.ExecuteNonQuery();
+                MessageBox.Show("Data jurusan terhapus");
+                siapkan_form_mode(true);
+                buka_grid(); bersihkan_form();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal Delete : " + ex.Message);
+            }*/
+            try
+            {
+                OracleCommand oupd = new OracleCommand("delete jurusan where " +
+                    "kode_jurusan='" + txtKodeJur.Text + "'"
+                    , conn);
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                oupd.ExecuteNonQuery();
+                MessageBox.Show("Data jurusan terhapus");
+                siapkan_form_mode(true);
+                buka_grid(); bersihkan_form();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal Delete : " + ex.Message);
+            }
+            
+        }
+
+        private void DG_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtKodeJur.Text = DT.Rows[e.RowIndex].ItemArray[0].ToString();
+            txtNamaJur.Text = DT.Rows[e.RowIndex].ItemArray[1].ToString();
+            cmbKepalaJur.SelectedIndex = cari_idx_kajur(DT.Rows[e.RowIndex].ItemArray[2].ToString());
+            if (DT.Rows[e.RowIndex].ItemArray[4].ToString() == "Y")
+                rbAktif.Checked = true;
+            else rbTidakAktif.Checked = true;
+            siapkan_form_mode(false);
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (rbTidakAktif.Checked) status = "N";
+            else status = "Y";
+            if (txtKodeJur.Text == "") MessageBox.Show("Isi Kode Jurusan");
+            else if (txtNamaJur.Text == "") MessageBox.Show("Isi Nama Jurusan");
+            else if (cmbKepalaJur.SelectedIndex == -1) MessageBox.Show("Pilih Kepala Jurusan");
+            else
+            {
+                try
+                {
+                    OracleCommand oins = new OracleCommand("insert into jurusan values(" +
+                  "'" + txtKodeJur.Text + "'," +
+                  "'" + DTKAJUR.Rows[cmbKepalaJur.SelectedIndex].ItemArray[0].ToString() + "'," +
+                  "'" + txtNamaJur.Text + "'," +
+                  "'" + status + "')"
+                  , conn);
+                    oins.ExecuteNonQuery();
+                    MessageBox.Show("1 data jurusan terbuat");
+                    siapkan_form_mode(true);
+                    buka_grid(); bersihkan_form();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal Insert : " + ex.Message);
+                }
+                
+            }
+        }
+
+    }
+}
