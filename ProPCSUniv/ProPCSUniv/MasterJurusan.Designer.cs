7726f99f40d54b5f8085625370fd8a0763f49142
Farrell Gunawan <39336308+FarrellG99@users.noreply.github.com>
2019-05-15 00:33:04 +0700

    Ganti Project PCS to Yang lebih Jauh

---
+++ ProPCSUniv/ProPCSUniv/MasterJurusan.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace ProPCSUniv
+{
+    partial class MasterJurusan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.DG = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rbTidakAktif = new System.Windows.Forms.RadioButton();
+            this.rbAktif = new System.Windows.Forms.RadioButton();
+            this.cmbKepalaJur = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtNamaJur = new System.Windows.Forms.TextBox();
+            this.txtKodeJur = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCari = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DG)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Controls.Add(this.DG);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btnCari);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(608, 438);
+            this.panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Search";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(85, 54);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(411, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // DG
+            // 
+            this.DG.AllowUserToAddRows = false;
+            this.DG.AllowUserToDeleteRows = false;
+            this.DG.BackgroundColor = System.Drawing.Color.White;
+            this.DG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DG.Location = new System.Drawing.Point(26, 84);
+            this.DG.Name = "DG";
+            this.DG.ReadOnly = true;
+            this.DG.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DG.Size = new System.Drawing.Size(564, 150);
+            this.DG.TabIndex = 4;
+            this.DG.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DG_CellClick);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.rbTidakAktif);
+            this.panel2.Controls.Add(this.rbAktif);
+            this.panel2.Controls.Add(this.cmbKepalaJur);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.btnDelete);
+            this.panel2.Controls.Add(this.btnUpdate);
+            this.panel2.Controls.Add(this.btnInsert);
+            this.panel2.Controls.Add(this.txtNamaJur);
+            this.panel2.Controls.Add(this.txtKodeJur);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(26, 240);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(564, 185);
+            this.panel2.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(19, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Status Mata Kuliah";
+            // 
+            // rbTidakAktif
+            // 
+            this.rbTidakAktif.AutoSize = true;
+            this.rbTidakAktif.Location = new System.Drawing.Point(347, 103);
+            this.rbTidakAktif.Name = "rbTidakAktif";
+            this.rbTidakAktif.Size = new System.Drawing.Size(76, 17);
+            this.rbTidakAktif.TabIndex = 4;
+            this.rbTidakAktif.Text = "Tidak Aktif";
+            this.rbTidakAktif.UseVisualStyleBackColor = true;
+            // 
+            // rbAktif
+            // 
+            this.rbAktif.AutoSize = true;
+            this.rbAktif.Checked = true;
+            this.rbAktif.Location = new System.Drawing.Point(161, 103);
+            this.rbAktif.Name = "rbAktif";
+            this.rbAktif.Size = new System.Drawing.Size(46, 17);
+            this.rbAktif.TabIndex = 3;
+            this.rbAktif.TabStop = true;
+            this.rbAktif.Text = "Aktif";
+            this.rbAktif.UseVisualStyleBackColor = true;
+            // 
+            // cmbKepalaJur
+            // 
+            this.cmbKepalaJur.FormattingEnabled = true;
+            this.cmbKepalaJur.Location = new System.Drawing.Point(142, 73);
+            this.cmbKepalaJur.Name = "cmbKepalaJur";
+            this.cmbKepalaJur.Size = new System.Drawing.Size(388, 21);
+            this.cmbKepalaJur.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Kepala Jurusan";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(423, 126);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(98, 41);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(228, 130);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(98, 41);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(20, 130);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(98, 41);
+            this.btnInsert.TabIndex = 5;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // txtNamaJur
+            // 
+            this.txtNamaJur.Location = new System.Drawing.Point(142, 44);
+            this.txtNamaJur.Name = "txtNamaJur";
+            this.txtNamaJur.Size = new System.Drawing.Size(388, 20);
+            this.txtNamaJur.TabIndex = 1;
+            // 
+            // txtKodeJur
+            // 
+            this.txtKodeJur.Location = new System.Drawing.Point(142, 18);
+            this.txtKodeJur.Name = "txtKodeJur";
+            this.txtKodeJur.Size = new System.Drawing.Size(63, 20);
+            this.txtKodeJur.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Nama Jurusan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Kode Jurusan";
+            // 
+            // btnCari
+            // 
+            this.btnCari.Location = new System.Drawing.Point(515, 48);
+            this.btnCari.Name = "btnCari";
+            this.btnCari.Size = new System.Drawing.Size(75, 30);
+            this.btnCari.TabIndex = 1;
+            this.btnCari.Text = "Cari";
+            this.btnCari.UseVisualStyleBackColor = true;
+            this.btnCari.Click += new System.EventHandler(this.btnCari_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(429, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MASTER JURUSAN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MasterJurusan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(640, 462);
+            this.Controls.Add(this.panel1);
+            this.Name = "MasterJurusan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ".: Jurusan :.";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MasterJurusan_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DG)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCari;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView DG;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox txtNamaJur;
+        private System.Windows.Forms.TextBox txtKodeJur;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbKepalaJur;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton rbTidakAktif;
+        private System.Windows.Forms.RadioButton rbAktif;
+    }
+}
